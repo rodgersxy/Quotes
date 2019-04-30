f4b3704f42baf6ed842cd9640d2a1aa002169bf4
rodgers <rodgersny99@gmail.com>
2019-04-30 10:28:39 +0300

    changes on the date count pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,18 +8,18 @@ export class DateCountPipe implements PipeTransform {
   transform(value:any): number {
     let today: Date = new Date(); // the current date and time
     let todayWithNoTime: any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var dateDifference =Math.abs(value-todayWithNoTime)// return value in milliseconds
+    var dateDifference =Math.abs(todayWithNoTime-value)// return value in milliseconds
     const secondsInADay =86400; //60 seconds*60 minutes in an hour *24 hours
 
 
     var dateDifferenceSeconds=dateDifference*0.001; // convert into secods
     var dateCounter = dateDifferenceSeconds/secondsInADay;
 
-    if (dateCounter >=1 && value > todayWithNoTime){
+    // if (dateCounter >=1 && value > todayWithNoTime){
       return dateCounter;
-    }else{
-      return 0;
-    }
+    // }else{
+    //   return 0;
+    // }
     
   }
 }
